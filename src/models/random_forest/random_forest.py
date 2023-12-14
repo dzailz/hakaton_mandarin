4dad689c04b7da19bc56281502caa45101ba6218
Vladimir Katin <katin.v.v@gmail.com>
2023-12-15 00:11:24 +0300

    try default SVMSMOTE

--- src/models/random_forest/random_forest.py
+++ src/models/random_forest/random_forest.py
@@ -1,6 +1,6 @@
 from typing import Any, Literal, Mapping, Sequence
 
-from imblearn.over_sampling import SMOTEN
+from imblearn.over_sampling import SVMSMOTE
 from pandas import DataFrame
 from sklearn.ensemble import RandomForestClassifier
 
@@ -90,6 +90,6 @@ class RandomForest(RandomForestClassifier):
         y = df[self.bank_decision]
 
         # Apply SMOTE
-        X_resampled, y_resampled = SMOTEN().fit_resample(X, y)
+        X_resampled, y_resampled = SVMSMOTE().fit_resample(X, y)
         self.X_resampled = X_resampled
         self.y_resampled = y_resampled
